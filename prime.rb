243eda36d94ecae1f8c20e8e665e2e3726a3ba0f
afteralec <github email address>
2020-07-19 18:39:33 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+  # A prime number is a number that is only divisible, without remainder, by itself or by 1
+
+  return false if number <= 0 || number == 1
+
+  !(1..number).to_a.find { |n| ((number % n) == 0) && n != 1 && n != number }
+end
